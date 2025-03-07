cfb65b631b4f69fba8c35bb68806ec3972d14b9f
naourk <sofiamikaelafrancisco@gmail.com>
2025-03-07 19:01:38 +0400

    add bingo caller feature

--- bingo.js
+++ bingo.js
@@ -1,6 +1,7 @@
 let gridSize = 5; // default grid size
 let pulledNumbers = []; // store pulled numbers
-let selectedMode = generate80Ball; // default bingo mode
+let selectedMode = generate75Ball; // default bingo mode
+let gameover = false;
 
 function generateUnique(low, interval, size) {
 	let arr = [];
@@ -16,7 +17,7 @@ function generateUnique(low, interval, size) {
 	return arr;
 }
 
-function generate80Ball() {
+function generate75Ball() {
 	let B = [],
 		I = [],
 		N = [],
@@ -37,7 +38,10 @@ function generate80Ball() {
 	for (let i = 0; i < gridSize; i++) {
 		for (let j = 0; j < bingoData.length; j++) {
 			if (j === 2 && i === 2) {
-				bingoDiv.innerHTML += "<div class='card' style='font-size:40px'>" + "<i class='bi bi-star-fill'></i>" + "</div>"; // add free space to bingo div
+				bingoDiv.innerHTML +=
+					"<div class='card' style='font-size:40px'>" +
+					"<i class='bi bi-star-fill'></i>" +
+					"</div>"; // add free space to bingo div
 				continue;
 			} else {
 				bingoDiv.innerHTML += "<div class='card'>" + bingoData[j][i] + "</div>"; // add card to bingo div
@@ -57,15 +61,51 @@ function generateCustom(bingoData) {
      */
 }
 
-function pullNumber() {
-	do {
-		let num = Math.floor(Math.random() * 75) + 1;
-		if (pulledNumbers.includes(num)) {
-			return num; // change to editing inner html
-		} else {
-			break;
-		}
-	} while (pulledNumbers.size < 75);
+function pullNumber75() {
+
+	let num;
+	let letter;
+    const pulledballs = document.getElementById("pulledballs");
+    const currentletter = document.getElementById("letter");
+	const currentnum = document.getElementById("number");
+
+    while (true) {
+        num = Math.floor(Math.random() * 75) + 1;
+        if (!pulledNumbers.includes(num)) {
+            pulledNumbers.push(num);
+            break;
+        }
+    }
+
+	if (num >= 1 && num <= 15) {
+		letter = "B";
+	} else if (num >= 16 && num <= 30) {
+		letter = "I";
+	} else if (num >= 31 && num <= 45) {
+		letter = "N";
+	} else if (num >= 46 && num <= 60) {
+		letter = "G";
+	} else if (num >= 61 && num <= 75) {
+		letter = "O";
+	}
+
+	const newBall = 
+		"<div class='outerball'> \
+			<div class='innerball'> \
+				<div class='text'> \
+					<p class='letter'>" + letter + "</p> \
+					<p class='number'>" + num + "</p> \
+				</div> \
+			</div> \
+		</div>";
+	pulledballs.innerHTML = newBall + pulledballs.innerHTML; // prepend new ball
+
+	currentletter.innerHTML = letter;
+	currentnum.innerHTML = num;
+}
+
+function showPrevious() {
+	document.getElementById("pulledballs").classList.toggle("toggled");
 }
 
 /* ---------------------------------- to do ---------------------------------- */
@@ -79,30 +119,37 @@ function pullNumber() {
 // add feature to save and load custom bingo card data
 // add feature to make middle card free space
 // add custom header feature
-// add panel to show pulled numbers
 // add multiplayer feature (servers/rooms?) + chat + account feature
 // add different bingo patterns (4 corners, X, etc.) + custom pattern +
 // add different bingo game types (75-ball, 80-ball, 90-ball, 30-ball) + custom mode
+// add ball color randomizer when generated
 
 document.addEventListener("DOMContentLoaded", (event) => {
 	selectedMode();
 
-	document.querySelectorAll(".card").forEach((card) => { // why inside this event listener?
+	document.querySelectorAll(".card").forEach((card) => {
+		// why inside this event listener?
 		card.addEventListener("click", () => {
 			card.classList.toggle("toggled");
 		});
 	});
 
-    document.getElementById("arrow").addEventListener("click", () => {
-        const panel = document.getElementById("panel");
-        const container = document.getElementById("container")
+	document.getElementById("arrow").addEventListener("click", () => {
+		const panel = document.getElementById("panel");
+		const container = document.getElementById("container");
 
-        panel.classList.toggle("collapsed");
-        container.classList.toggle("fullscreen");
-    });
+		panel.classList.toggle("collapsed");
+		container.classList.toggle("fullscreen");
+	});
 
-    document.getElementById("menu").addEventListener("click", () => {
-        panel.classList.toggle("collapsed");
-        container.classList.toggle("fullscreen");
+	document.getElementById("menu").addEventListener("click", () => {
+		panel.classList.toggle("collapsed");
+		container.classList.toggle("fullscreen");
+	});
+
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Space" && pulledNumbers.length < 75) {
+            pullNumber75();
+        }
     });
 });
